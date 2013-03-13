a029d77a52ce652943b70b2bebb329ff0540f2b2
Daniel Schulz <danielschulz2005@hotmail.com>
2013-03-13 18:12:52 +0100

    added basic test

---
+++ Macadamia/src/main/java/de/novensa/cs/performance/macadamia/Macadamia.java
@@ -0,0 +1,15 @@
+package de.novensa.cs.performance.macadamia;
+
+import java.util.Collections;
+
+/**
+ * The data structures holding instance.
+ *
+ * @author Daniel Schulz
+ */
+public class Macadamia {
+
+    public static void main(String[] args) {
+        Collections.sort(null);
+    }
+}
---
+++ Macadamia/src/test/java/de/novensa/cs/performance/macadamia/MacadamiaExistTests.java
@@ -0,0 +1,24 @@
+package de.novensa.cs.performance.macadamia;
+
+import junit.framework.TestCase;
+import org.junit.*;
+
+/**
+ * The existing-within-index-tests are placed here.
+ *
+ * @author Daniel Schulz
+ */
+public class MacadamiaExistTests extends TestCase {
+
+    @Before
+    public void testBefore() throws Exception {
+    }
+
+    @Test
+    public void testName() throws Exception {
+    }
+
+    @After
+    public void testAfter() throws Exception {
+    }
+}
