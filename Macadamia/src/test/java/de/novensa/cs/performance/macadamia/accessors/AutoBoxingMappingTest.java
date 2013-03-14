b7c6867ab79622ff9acd24df19919844b28a4998
danielschulz2005@hotmail.com <danielschulz2005@hotmail.com>
2013-03-14 21:00:30 +0100

    static import only

--- Macadamia/src/test/java/de/novensa/cs/performance/macadamia/accessors/AutoBoxingMappingTest.java
+++ Macadamia/src/test/java/de/novensa/cs/performance/macadamia/accessors/AutoBoxingMappingTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import static de.novensa.cs.performance.macadamia.util.ClassCastPrediction.*;
 import static de.novensa.cs.performance.macadamia.util.Constants.OBJECT;
 import static de.novensa.cs.performance.macadamia.util.Constants.STRING;
+import static org.junit.Assert.assertEquals;
 
 /**
  * The test case for the auto boxing mapping.
@@ -17,34 +18,34 @@ public class AutoBoxingMappingTest extends AutoBoxingTestObjects {
     @Test
     public void testAutoBoxingMappings() {
 
-        Assert.assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(STRING, STRING));
-        Assert.assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(STRING, OBJECT));
-        Assert.assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(OBJECT, OBJECT));
-        Assert.assertEquals(CANNOT_BE_TOLD, sharedClassCache.isCastingPossible(OBJECT, STRING));
+        assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(STRING, STRING));
+        assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(STRING, OBJECT));
+        assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(OBJECT, OBJECT));
+        assertEquals(CANNOT_BE_TOLD, sharedClassCache.isCastingPossible(OBJECT, STRING));
 
-        Assert.assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(INST_INTEGER, PRIM_INT));
-        Assert.assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(PRIM_INT, INST_INTEGER));
-        Assert.assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(INST_FLOAT, PRIM_FLOAT));
-        Assert.assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(PRIM_FLOAT, INST_FLOAT));
+        assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(INST_INTEGER, PRIM_INT));
+        assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(PRIM_INT, INST_INTEGER));
+        assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(INST_FLOAT, PRIM_FLOAT));
+        assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(PRIM_FLOAT, INST_FLOAT));
 
-        Assert.assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(INST_FLOAT, OBJECT));
-        Assert.assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(INST_INTEGER, OBJECT));
-        Assert.assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(PRIM_FLOAT, OBJECT));
-        Assert.assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(PRIM_INT, OBJECT));
+        assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(INST_FLOAT, OBJECT));
+        assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(INST_INTEGER, OBJECT));
+        assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(PRIM_FLOAT, OBJECT));
+        assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(PRIM_INT, OBJECT));
 
-        Assert.assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(INST_BOOLEAN, PRIM_BOOLEAN));
-        Assert.assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(PRIM_BOOLEAN, INST_BOOLEAN));
+        assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(INST_BOOLEAN, PRIM_BOOLEAN));
+        assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(PRIM_BOOLEAN, INST_BOOLEAN));
 
-        Assert.assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(INST_BOOLEAN, INST_BOOLEAN));
-        Assert.assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(PRIM_BOOLEAN, PRIM_BOOLEAN));
+        assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(INST_BOOLEAN, INST_BOOLEAN));
+        assertEquals(POSSIBLE, sharedClassCache.isCastingPossible(PRIM_BOOLEAN, PRIM_BOOLEAN));
 
-        Assert.assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(INST_INTEGER, PRIM_FLOAT));
-        Assert.assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(INST_FLOAT, PRIM_INT));
+        assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(INST_INTEGER, PRIM_FLOAT));
+        assertEquals(LIKELY_POSSIBLE, sharedClassCache.isCastingPossible(INST_FLOAT, PRIM_INT));
 
 
-        Assert.assertEquals(IMPOSSIBLE, sharedClassCache.isCastingPossible(INST_BOOLEAN, PRIM_INT));
-        Assert.assertEquals(IMPOSSIBLE, sharedClassCache.isCastingPossible(INST_BOOLEAN, INST_INTEGER));
-        Assert.assertEquals(IMPOSSIBLE, sharedClassCache.isCastingPossible(PRIM_BOOLEAN, PRIM_INT));
-        Assert.assertEquals(IMPOSSIBLE, sharedClassCache.isCastingPossible(PRIM_BOOLEAN, INST_INTEGER));
+        assertEquals(IMPOSSIBLE, sharedClassCache.isCastingPossible(INST_BOOLEAN, PRIM_INT));
+        assertEquals(IMPOSSIBLE, sharedClassCache.isCastingPossible(INST_BOOLEAN, INST_INTEGER));
+        assertEquals(IMPOSSIBLE, sharedClassCache.isCastingPossible(PRIM_BOOLEAN, PRIM_INT));
+        assertEquals(IMPOSSIBLE, sharedClassCache.isCastingPossible(PRIM_BOOLEAN, INST_INTEGER));
     }
 }
